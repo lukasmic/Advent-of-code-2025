feacfad4b4a66e1e6da3266c0ef3db65d7d3328f
lukasmic <lukas.miciulis@outlook.com>
2025-12-07 17:06:37 +0200

    4

---
+++ 4/4.5.cs
@@ -0,0 +1,94 @@
+var grid = new PaperRollGrid();
+
+using (var file = new StreamReader("4/4-input.txt"))
+{
+    string? line;
+    while ((line = file?.ReadLine()) != null)
+    {
+        grid.AddRow(line);
+    }
+
+    grid.Forklift();
+
+    // grid.GetRollNeighbourCount(0, 0);
+    // grid.GetRollNeighbourCount(0, 2);
+    // grid.PrintTheRug();
+}
+
+internal class PaperRollGrid
+{
+    private List<List<bool>> Map = [];
+    private short RowsCount = 0;
+    private short ForkliftedRolls = 0;
+
+    internal void AddRow(string newRow)
+    {
+        Map.Add([]);
+        for (int i = 0; i < newRow.Length; i++)
+        {
+            Map[RowsCount].Add(newRow[i] == '@');
+        }
+        RowsCount++;
+    }
+
+    internal void Forklift()
+    {
+        for (short y = 0; y < Map.Count; y++)
+        {
+            for (short x = 0; x < Map[y].Count; x++)
+            {
+                if (!Map[y][x]) continue;
+
+                var neighbourCount = GetPaperRollNeighbourCount(y, x);
+
+                if (neighbourCount < 4)
+                {
+                    ForkliftedRolls++;
+                }
+            }
+        }
+
+        Console.WriteLine("Forklifted " + ForkliftedRolls + " rolls");
+    }
+
+    private byte GetPaperRollNeighbourCount(short rollY, short rollX)
+    {
+        byte neighbourCount = 0;
+
+        for (int y = rollY - 1; y <= rollY + 1; y++)
+        {
+            if (y < 0 || y >= Map.Count) continue;
+
+            for (int x = rollX - 1; x <= rollX + 1; x++)
+            {
+                if (x == rollX && y == rollY // Don't count self lol
+                    || x < 0
+                    || x >= Map[y].Count)
+                    continue;
+
+                if (Map[y][x])
+                {
+                    neighbourCount++;
+                }
+            }
+
+        }
+
+        // Console.WriteLine("Roll " + rollY + ":" + rollX + "neighbour count: " + neighbourCount);
+        return neighbourCount;
+    }
+
+    public void PrintTheRug()
+    {
+        Console.WriteLine(Map[0].Count);
+        Console.WriteLine(Map.Count);
+        foreach (var row in Map)
+        {
+            Console.WriteLine();
+            foreach (var column in row)
+            {
+                Console.Write(column ? "@" : ".");
+            }
+        }
+    }
+}
